7c0acef2e63ed57b7c15f7a648c0ffb8577b7fc5
Alimary28 <alexandru28.pa@gmail.com>
2020-05-10 12:11:58 +0300

    Solution using a Character class for games

--- FirstWebApp/Models/CharacterItem.cs
+++ FirstWebApp/Models/CharacterItem.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
 namespace FirstWebApp.Models
 
@@ -11,8 +12,11 @@ namespace FirstWebApp.Models
         public long Id { get; set; }
         public string Name { get; set; }
         public int Defence { get; set; }
+        [Range(100.0,2000.0)]
         public int Strength { get; set; }
+        [Range(100.0,2000.0)]
         public int Intelligence { get; set; }
+        [Range(100.0,2000.0)]
 
     }
 }
